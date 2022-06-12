86c1cf5f25f2faf79463ae4c92904bb2dad9c6a4
Roger Fiallos <roger.mfs@hotmail.com>
2022-06-12 11:01:15 +0200

    MarRoverMission: Planet obstacle detector checker.

---
+++ src/Rover/Application/Service/PlanetObstacleDetector.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Rover\Application\Service;
+
+use App\Rover\Domain\Exception\ObstacleDetectedException;
+use App\Rover\Domain\Planet;
+use App\Rover\Domain\Position;
+
+final class PlanetObstacleDetector
+{
+    public function __invoke(
+        Planet $planet,
+        Position $nextPosition,
+    ): void
+    {
+        if ($planet->hasObstacleIn($nextPosition)) {
+            throw new ObstacleDetectedException($nextPosition);
+        }
+    }
+}
---
+++ src/Rover/Domain/Exception/ObstacleDetectedException.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Rover\Domain\Exception;
+
+use App\Rover\Domain\Position;
+use DomainException;
+use Throwable;
+
+final class ObstacleDetectedException extends DomainException
+{
+    public function __construct(
+        Position $position,
+        ?Throwable $previous = null
+    )
+    {
+        parent::__construct(
+            sprintf('Obstacle detected in position [%s].', (string)$position),
+            0,
+            $previous
+        );
+    }
+}
