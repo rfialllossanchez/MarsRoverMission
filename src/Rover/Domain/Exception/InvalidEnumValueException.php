1fe93e3b92d3401a9c1a79581eacdfc8486aca9d
Roger Fiallos <roger.mfs@hotmail.com>
2022-06-16 19:53:29 +0200

    Adding enum VO.

---
+++ src/Rover/Domain/Exception/InvalidEnumValueException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Rover\Domain\Exception;
+
+use DomainException;
+use Throwable;
+
+class InvalidEnumValueException extends DomainException
+{
+    public function __construct($message, Throwable $previous = null)
+    {
+        parent::__construct($message, 0, $previous);
+    }
+}
---
+++ src/Rover/Domain/ValueObject/EnumValueObject.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Rover\Domain\ValueObject;
+
+use App\Rover\Domain\Exception\InvalidEnumValueException;
+
+abstract class EnumValueObject
+{
+    /** @var string[] */
+    protected array $allowedValues = [];
+    protected string $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+
+        $this->ensureValueIsAllowed();
+    }
+
+    public function __toString(): string
+    {
+        return $this->value();
+    }
+
+    public function value(): string
+    {
+        return $this->value;
+    }
+
+    private function ensureValueIsAllowed(): void
+    {
+        if (!in_array($this->value, $this->allowedValues)) {
+            throw new InvalidEnumValueException("Value {$this->value} is not supported.");
+        }
+    }
+}
